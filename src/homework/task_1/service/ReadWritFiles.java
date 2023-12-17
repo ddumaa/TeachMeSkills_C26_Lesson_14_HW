67caa3fcfd5395b43eb381a2db7c6e9543d54baf
ddumaa <dimaduma62@gmail.com>
2023-12-17 22:29:43 +0300

    fix

--- src/homework/task_1/service/ReadWritFiles.java
+++ src/homework/task_1/service/ReadWritFiles.java
@@ -9,22 +9,17 @@ import java.util.Date;
 public class ReadWritFiles {
     public static  void readingWritingFiles(String path){
         Log.EXECUTION_LOG(new Date(),"Start reading file\n");
-        try{
-            BufferedReader reader = new BufferedReader(new FileReader(path));
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
             String line = reader.readLine();
             while (line != null) {
                 System.out.println(line);
                 Validator.validationNameDocuments(line);
                 line = reader.readLine();
             }
-            //тут закрывается ридер?
-            reader.close();
         } catch (FileNotFoundException e) {
             Log.ERROR_LOG(new Date(), e.getMessage());
         } catch (IOException e) {
             Log.ERROR_LOG(new Date(), e.getMessage());
-        } finally {
-            //не смог закрыть его тут, нет метода.
         }
         Log.EXECUTION_LOG(new Date(), "End of file reading\n");
     }
